bc314032bb0192b8db906e17845e9b661a6dd408
Ubuntu <ubuntu@ip-172-31-45-41.us-west-2.compute.internal>
2013-08-02 04:33:06 +0000

    added restler

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -55,18 +56,26 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var urlOption = function(result){
+    var checkJson = checkHtmlFile(result, program.checks);}
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_address>', 'URL for html file')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    if (program.url){rest.get(program.url).on('complete', urlOption(result));}
+//    if (program.url){console.log(program.url);}
+    else {var checkJson = checkHtmlFile(program.file, program.checks);}
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
